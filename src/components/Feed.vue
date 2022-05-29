33e1ecd7741eaf159bf7434b86d3031e9ff57a32
MiguelAngelLiera <miguelliera@ciencias.unam.mx>
2022-05-28 22:24:49 -0500

    Añadido el funcionamiento de publicación, sin multimedia

---
+++ src/components/Feed.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+    <NavBarInicio/>
+    <section>
+        <div class="card my-card" >
+            <div class=card-header>
+                <h2>Noticias</h2>
+            </div>
+            <div class="card-body">
+                <button class="btn btn-primary mb-2" v-on:click="publicar">Crear publicación</button>
+            </div>
+        </div>
+    </section>
+    </div>
+</template>
+
+<script>
+
+import router from '../router';
+export default {
+    name: 'Feed',
+    data: function(){
+        return {
+        }
+    },
+    methods: {
+        publicar() {
+            router.push("/ui/publicacion")
+        }
+    }
+}
+</script>
--- src/components/Publicacion.vue
+++ src/components/Publicacion.vue
@@ -3,17 +3,109 @@
     <section>
         <div class="card my-card" >
             <div class=card-header>
-                <h2>Publicación</h2>
+                <h2>Crear publicación</h2>
+                <button class="b_cerrar" size="sm" variant="outline-danger" @click="cerrar">Cerrar</button>
+            </div>
+            <div class="e_privacidad">
+                <button
+                    class="btn-main"
+                    @click="toggle"
+                    :class="[b_publica,
+                    es_publica ? 'active' : '']"
+                >Publica</button>
+                <button
+                    class="btn-main"
+                    @click="toggle"
+                    :class="[b_publica,
+                    !es_publica ? 'active' : '']"
+                >Privada</button>
             </div>
             <div class="card-body">
                 <label>
-                    Texto
-                    <input type="text" v-model="texto" class="form-control"/>
+                    <b-row md="4">
+                        <b-col md="120">
+                            <b-form-textarea type="text" v-model="texto" rows="3" size="lg" placeholder="¿Qué estás pensando...?"></b-form-textarea>
+                        </b-col>
+                    </b-row>
                 </label>
-                
-                <button class="btn btn-primary mb-2" v-on:click="publicar">Publicar</button>
+                <div>
+                    <b-button class="b_multimedia" @click="$bvModal.show('modal-multimedia')">Agregar contenido multimedia</b-button>
+
+                    <b-modal id="modal-multimedia" centered hide-footer>
+                         <template #modal-header>
+                            <!-- Emulate built in modal header close button action -->
+                                <b-button size="sm" class="b_cerrar-1" @click="$bvModal.hide('modal-multimedia')">
+                                    Cerrar
+                                </b-button>
+                            <h5>Agregar contenido Multimedia</h5>
+                        </template>
+                        <div class="b_mod">
+                            <b-button @click="$bvModal.show('modal-imagen')" class="btn btn-primary mb-2">Imagenes</b-button>
+                        </div>
+                        <div class="b_mod">
+                            <b-button @click="$bvModal.show('modal-video')" class="btn btn-primary mb-2">Video</b-button>
+                        </div>
+                        <div class="b_mod">
+                            <b-button @click="$bvModal.show('modal-gif')" class="btn btn-primary mb-2">Gif</b-button>
+                        </div>
+                        
+                    </b-modal>
+                    <b-modal id="modal-imagen" centered title="Selecciona el tipo de fuente de tu contenido multimedia" hide-footer>
+                        <template #modal-header>
+                            <!-- Emulate built in modal header close button action -->
+                                <b-button size="sm" class="b_cerrar-1" @click="$bvModal.hide('modal-imagen')">
+                                    Cerrar
+                                </b-button>
+                            <h5>Agregar contenido Multimedia</h5>
+                        </template>
+                        <div class="b_mod">
+                            <b-button>Subir desde el ordenador</b-button>
+                        </div>
+                        <div class="b_mod">
+                            <b-button>Elegir del perfil</b-button>
+                        </div>
+                    </b-modal>
+                    <b-modal id="modal-video" centered title="Selecciona el tipo de fuente de tu contenido multimedia" hide-footer>
+                        <template #modal-header>
+                            <!-- Emulate built in modal header close button action -->
+                                <b-button size="sm" class="b_cerrar-1" @click="$bvModal.hide('modal-video')">
+                                    Cerrar
+                                </b-button>
+                            <h5>Agregar contenido Multimedia</h5>
+                        </template>
+                        <div class="b_mod">
+                            <b-button>Subir desde el ordenador</b-button>
+                        </div>
+                        <div class="b_mod">
+                            <b-button>Elegir del perfil</b-button>
+                        </div>
+                    </b-modal>
+                    <b-modal id="modal-gif" centered title="Selecciona el tipo de fuente de tu contenido multimedia" hide-footer>
+                        <template #modal-header>
+                            <!-- Emulate built in modal header close button action -->
+                                <b-button size="sm" class="b_cerrar-1" @click="$bvModal.hide('modal-gif')"> <!--variant="outline-danger" -->
+                                    Cerrar
+                                </b-button>
+                            <h5>Agregar contenido Multimedia</h5>
+                        </template>
+                        <div class="b_mod">
+                            <b-button>Subir desde el ordenador</b-button>
+                        </div>
+                        <div class="b_mod">
+                            <b-button>Elegir del perfil</b-button>
+                        </div>
+                    </b-modal>
+                </div>
+                <div>
+                    <label class="b_publicar">
+                        <button class="btn btn-primary mb-2" v-on:click="publicar">Publicar</button>
+                    </label>
+                    
+                    
+                </div>
             </div>
         </div>
+        
         <b-modal id="error" :title="titulo_error">
             <p>{{msj_error}}</p>
         </b-modal>
@@ -21,11 +113,16 @@
             <p>{{body_aviso}}</p>
         </b-modal>
     </section>
+    <!--eetiqueta input, file en html y eso mandarlo al scripy hacer un evento, una interaccion entre el navegador. @change, le paso una funcion que 
+    //se ejecute cuando el usuario suba el archivo from data, objeto de js-->
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import SERVER from '../sites.js'
+import store from '../store/index';
+import router from '../router';
 export default {
     name: 'Publicacion',
     components: {
@@ -34,25 +131,71 @@ export default {
     data: function(){
         return {
             texto: "",
-
+            es_publica: true,
+            c_multimedia: "",
+            b_publica: '',
+            dialog_multimedia: false
         }
-    },
-    components: {
-
+        //que son los componentres, ps los otros componentes que tengo en la carpeta components
     },
     methods: {
+        aux(){
+            console.log(store.state.id);
+        },
         publicar(){
-          axios.post(SERVER + '/usuario/publicacion/', {
-                texto: this.texto,
+            var f = new Date();
+            var mes = f.getMonth() + 1
+            mes = mes + ''
+            if(mes.length == 1){
+                mes = '0' + mes
+            }
+            var dia = f.getDate()
+            dia = dia + ''
+            if(dia.length == 1){
+                dia = '0' + dia
+            }
+            var fecha = f.getFullYear() + "-" + mes + "-" + dia + " " + f.getHours() + ':' + f.getMinutes() + ':' + f.getSeconds();
+            console.log(fecha)
+        axios.post(SERVER + '/feed/publicacion/', {
+                // multimedia = //lista?
+                id: "null",
+                usuarioId: store.state.id,   
+                textoPlano: this.texto,
+                fechaCreacion: fecha,
+                esPublica: this.es_publica
           }).then(response => {
-                
+                //que significa esto en el archivo original
+                //promesa, es lo que va a hacer esta madre cuando el sercidor responda
+                store.commit("set_jwt", response.data.jwt);//setea el valor del jwt segun lo que nos respondió el servidor
+                console.log(store.state.jwt);
+                router.push("/ui/feed")
             }).catch(error => {
-                
+                //los errores
+                router.push("/ui/publicacion")
+                console.log(error.response.status);
+                this.msj_error = error.response.data.Accion;
+                console.log(error.response.data);
+                this.$bvModal.show("error");
+                this.titulo_error = error.response.data.Descripcion;
            });
-
+        },
+        toggle() {
+            this.es_publica = !this.es_publica;
+        },
+        cerrar() {
+            router.push("/ui/login")
         }
+        // c_multimedia(){
+            
+        // }
     }
+    //cuadno guardemos imagenes es ap.destination, pedimos como /nombre.imagen
 }
+//como correr la aplicacion npm run serve desde terminal
+//un componente por cada ventana? no
+//componentes: publicacion, agregar mulrimedia(foto, video o multimedia), la fuente(como acceder a archivos de la computadora), cuando ya seleccionemos
+//la imagen, la mandamos al componente publicacion? no, unosolamente dos, publ y noticias
+//crear un ultimo componente que sea el feed? con crear publicaciones si
 </script>
 
 <style scoped>
@@ -60,7 +203,69 @@ export default {
     margin: 0;
     padding: 0;
   }
-  
+  .b_mod {
+      display: flex;
+      justify-content: center;
+  }
+  .b_cerrar {
+      position: relative;
+      top: -55px; left: 650px;
+      background-color: rgb(204, 0, 0);
+      color: rgb(255, 255, 255);
+  }
+  .b_cerrar-1 {
+      background-color: rgb(204, 0, 0);
+      color: rgb(255, 255, 255);
+  }
+  .card-header-2{
+      font-size: 15px;
+  }
+  .b_publicar{
+      position: relative;
+      bottom: 60px; left: 620px;
+  }
+  .b_multimedia{
+      position: relative;
+      bottom: 0px; right: 0px;
+  }
+  .e_privacidad{
+      position: relative;
+      top: 0px; left: 533px;
+  }
+  .btn-main {
+  background-color: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+  font-family: 'Raleway', sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  letter-spacing: 1.3px;
+  padding: 0.2px 3px; /*medidas del boton*/
+  text-decoration: none;
+  text-transform: lowercase;
+}
+
+.btn-main:hover {
+  background-color: rgb(6, 103, 250);
+  color: black;
+  font-family: 'Raleway', sans-serif;
+  font-weight: 400;
+  font-size: 18px;
+  letter-spacing: 1.3px;
+  padding: 0.2px 3px;
+  text-decoration: none;
+  text-transform: lowercase;
+}
+.active{
+    background-color: rgb(6, 103, 250);
+    color: black;
+    font-family: 'Raleway', sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+    letter-spacing: 1.3px;
+    padding: 0.2px 3px;
+    text-decoration: none;
+    text-transform: lowercase;
+}
       .my-card{
           width: 40vw;
       }
@@ -86,6 +291,7 @@ export default {
       button {
           margin-top: 2%;
       }
+      
       .footer {
           display: flex;
           align-items: center;
@@ -104,4 +310,6 @@ export default {
               width: 80vw;
           }
       }
+
+      
 </style>
